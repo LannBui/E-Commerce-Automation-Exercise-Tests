bb14c140548aff9d7cfb03c427a75e333d74a1c9
Bui Thi Lan <bui.lan@globant.com>
2025-06-16 13:48:32 +0700

    update

--- src/test/java/org/ecommerceautomationexercise/tests/QuantityTest.java
+++ src/test/java/org/ecommerceautomationexercise/tests/QuantityTest.java
@@ -1,7 +1,5 @@
 package org.ecommerceautomationexercise.tests;
 
-import io.qameta.allure.Epic;
-import io.qameta.allure.Feature;
 import org.ecommerceautomationexercise.base.BaseTest;
 import org.ecommerceautomationexercise.pages.CartPage;
 import org.ecommerceautomationexercise.pages.HomePage;
@@ -11,8 +9,7 @@ import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
 public class QuantityTest extends BaseTest {
-//    @Epic("E-Commerce")
-//    @Feature("Cart")
+
     @Test (dataProvider = "quantityData", dataProviderClass = utils.CSVReaderUtil.class, groups = {"regression"})
     public void testQuantityInCart (String quantity, boolean shouldSucceed) {
         driver.get("https://automationexercise.com");
